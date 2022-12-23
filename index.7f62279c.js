a76af800cf2314519bad9f39a34b7b7af1a2928a
Olga-Dames <113420629+Olga-Dames@users.noreply.github.com>
2022-12-23 14:33:53 +0000

    Deploying to gh-pages from @ Olga-Dames/teamProject17@a7eb8ebe1866b925387e963ed59ef4755dc3ce53 🚀

--- index.7f62279c.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),modal:document.querySelector("[data-modal]")};e.openModalBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden")}))})();
-//# sourceMappingURL=index.7f62279c.js.map
